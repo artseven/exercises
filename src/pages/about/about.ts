02c0655a696210a1fcc4091851a8ad35b3afefbb
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-20 16:34:23 -0400

    tapping and pressing implemented

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,9 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+  tap: number = 0;
+  press: number = 0;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onElementTapped() {
+    this.tap++;
+    console.log(this.tap);
+  }
+
+  onElementPressed() {
+    console.log('I was pressed');
+    this.press++;
   }
 
 }

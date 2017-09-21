178e3912fde96295ceb46d58d2db428b58d2827a
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-21 13:25:59 -0400

    refactoring code

--- src/components/reset.component.ts
+++ src/components/reset.component.ts
@@ -5,33 +5,22 @@ import { Component, EventEmitter, Output } from '@angular/core';
     template: `
     <ion-row>
         <ion-col col-4>
-            <button ion-button small color="danger" (click)="onResetAll()">Reset All</button>
+            <button ion-button small block color="danger" (click)="onReset('all')">Reset All</button>
         </ion-col>
         <ion-col col-4>
-            <button ion-button small color="danger" (click)="onResetTaps('taps')">Reset "Taps"</button>
+            <button ion-button small block color="danger" (click)="onReset('taps')">Reset "Taps"</button>
         </ion-col>
         <ion-col col-4>
-            <button ion-button small color="danger" (click)="onResetPresses('presses')">Reset "Presses"</button>
+            <button ion-button small block color="danger" (click)="onReset('presses')">Reset "Presses"</button>
         </ion-col>
     </ion-row>
     `
 })
 export class ResetComponent {
-    @Output() change: EventEmitter<number> = new EventEmitter();
-    @Output() taps: EventEmitter<number> = new EventEmitter();
-    @Output() presses: EventEmitter<number> = new EventEmitter();
+    @Output() didReset = new EventEmitter<string>();
 
-    onResetAll() {
-        this.change.emit(0);
+    onReset(type: string) {
+        this.didReset.emit(type);
     }
 
-    onResetTaps() {
-        this.taps.emit(0);
-        console.log('I should reset taps to 0');
-    }
-
-    onResetPresses() {
-        this.presses.emit(0);
-        console.log('I should reset presses to 0');
-    }
 }
